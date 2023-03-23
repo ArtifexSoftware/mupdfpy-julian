f4a3904386454a5df011f9f8fff5f70a27f518b6
Julian Smith <julian.smith@artifex.com>
2023-03-23 13:54:17 +0000

    fitz/__init__.py: fixups for pymupdf 6a9c9d8175c307f "Multiple fixes".

--- fitz/__init__.py
+++ fitz/__init__.py
@@ -19003,30 +19003,28 @@ def util_ensure_widget_calc(annot):
     '''
     Ensure that widgets with /AA/C JavaScript are in array AcroForm/CO
     '''
-   pdf_obj *PDFNAME_CO=NULL;
-   fz_try(gctx) {
-       annot_obj = mupdf.pdf_annot_obj(annot.this)
-       pdf = mupdf.pdf_get_bound_document(annot_obj)
-       PDFNAME_CO = mupdf.pdf_new_name("CO")    # = PDF_NAME(CO)
-       acro = mupdf.pdf_dict_getl(  # get AcroForm dict
-               mupdf.pdf_trailer(pdf),
-               PDF_NAME('Root'),
-               PDF_NAME('AcroForm'),
-               )
+    annot_obj = mupdf.pdf_annot_obj(annot.this)
+    pdf = mupdf.pdf_get_bound_document(annot_obj)
+    PDFNAME_CO = mupdf.pdf_new_name("CO")    # = PDF_NAME(CO)
+    acro = mupdf.pdf_dict_getl(  # get AcroForm dict
+            mupdf.pdf_trailer(pdf),
+            PDF_NAME('Root'),
+            PDF_NAME('AcroForm'),
+            )
 
-       CO = mupdf.pdf_dict_get(acro, PDFNAME_CO)    # = AcroForm/CO
-       if not CO.this:
-           CO = mupdf.pdf_dict_put_array(acro, PDFNAME_CO, 2)
-       n = mupdf.pdf_array_len(CO)
-       found = 0
-       xref = mupdf.pdf_to_num(annot_obj)
-       for i in range(n):
-           nxref = mupdf.pdf_to_num(mupdf.pdf_array_get(CO, i))
-           if xref == nxref:
-               found = 1
-               break
-       if not found:
-           mupdf.pdf_array_push(CO, mupdf.pdf_new_indirect(pdf, xref, 0))
+    CO = mupdf.pdf_dict_get(acro, PDFNAME_CO)    # = AcroForm/CO
+    if not CO.this:
+        CO = mupdf.pdf_dict_put_array(acro, PDFNAME_CO, 2)
+    n = mupdf.pdf_array_len(CO)
+    found = 0
+    xref = mupdf.pdf_to_num(annot_obj)
+    for i in range(n):
+        nxref = mupdf.pdf_to_num(mupdf.pdf_array_get(CO, i))
+        if xref == nxref:
+            found = 1
+            break
+    if not found:
+        mupdf.pdf_array_push(CO, mupdf.pdf_new_indirect(pdf, xref, 0))
 
 
 def util_make_rect( *args):
@@ -20513,8 +20511,8 @@ TextWriter.fill_textbox     = utils.fill_textbox
 class FitzDeprecation(DeprecationWarning):
     pass
 
-VersionFitz = "1.19.0"
-VersionBind = "1.19.5"
+VersionFitz = "1.22.0" # MuPDF version.
+VersionBind = "1.22.0" # PyMuPDF version.
 VersionDate = "2022-02-01 00:00:01"
 version = (VersionBind, VersionFitz, "20220201000001")
 VersionDate2 = VersionDate.replace('-', '').replace(' ', '').replace(':', '')
