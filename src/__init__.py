2d7cc906e9b85a27e7454495cfaebed984855d9d
Julian Smith <julian.smith@artifex.com>
2023-05-31 17:43:57 +0100

    src/__init__.py: make JM_EscapeStrFromStr() behave like PyMuPDF.

--- src/__init__.py
+++ src/__init__.py
@@ -13355,8 +13355,30 @@ def JM_BinFromBuffer(buffer_):
     return ret
 
 def JM_EscapeStrFromStr(c):
-    # fixme: need to make this handle escape sequences.
-    return c
+    # `c` is typically from SWIG which will have converted a `const char*` from
+    # C into a Python `str` using `PyUnicode_DecodeUTF8(carray, static_cast<
+    # Py_ssize_t >(size), "surrogateescape")`.  This gives us a Python `str`
+    # with some characters encoded as a \0xdcXY sequence, where `XY` are hex
+    # digits for an invalid byte in the original `const char*`.
+    #
+    # This is actually a reasonable way of representing arbitrary
+    # strings from C, but we want to mimic what PyMuPDF does. It uses
+    # `PyUnicode_DecodeRawUnicodeEscape(c, (Py_ssize_t) strlen(c), "replace")`
+    # which gives a string containing actual unicode characters for any invalid
+    # bytes.
+    #
+    # We mimic this by converting the `str` to a `bytes` with 'surrogateescape'
+    # to recognise \0xdcXY sequences, then convert the individual bytes into a
+    # `str` using `chr()`.
+    #
+    # Would be good to have a more efficient way to do this.
+    #
+    assert isinstance(c, str)
+    b = c.encode('utf8', 'surrogateescape')
+    ret = ''
+    for bb in b:
+        ret += chr(bb)
+    return ret
 
 
 def JM_BufferFromBytes(stream):
